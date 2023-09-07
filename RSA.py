8afcad71e83a4197d7753642e9b26e869b9b1444
corey.cushnie <corey.cushnie@gmail.com>
2023-09-07 18:29:34 -0400

    File find fixes

--- RSA.py
+++ RSA.py
@@ -1,19 +1,21 @@
 import math
 import random
 import os
+import re
 
 _random = random.SystemRandom()
 
+
 class RSA:
     def __init__(self, s=64):
-
         # Initializes the keys.
         self.key = self.keygen(s)
+        self.currentDir = re.sub(r"/[^/]*\.py$", "", __file__)
 
     # Extended Euclidean algorithm.
     def ext_euclidean(self, x, y):
-
-        if (y == 0): return (x, 1, 0)
+        if y == 0:
+            return (x, 1, 0)
         d, p, q = self.ext_euclidean(y, x % y)
         s = q
         t = p - q * (x // y)
@@ -21,23 +23,22 @@ class RSA:
 
     # Prime factorizations returns in u and k.
     def primeFactorize(self, n):
-
         # Converts the large prime number into a string and returns n - 1 if the last digit is even.
         n = n - 1 if int(str(n)[-1]) % 2 != 0 else n
-        u, k = 1, 0 
+        u, k = 1, 0
 
         while n % 2 == 0:
             k += 1
             n /= 2
 
-        for i in range(3, int(math.sqrt(n))+1, 2):
+        for i in range(3, int(math.sqrt(n)) + 1, 2):
             while n % i == 0:
                 u *= i
                 n /= i
-                    
+
         if n > 2:
             u *= n
-        
+
         return int(u), k
 
     # Miller-Rabin Primality test
@@ -57,12 +58,12 @@ class RSA:
 
     # Power Modulus function, converts the exponent into binary and calculates the value for largers numbers.
     def squareMultiply(self, x, y, mod):
-        exp = format(y, 'b')
+        exp = format(y, "b")
         value = x
         for i in exp[1:]:
-            value = ((value * value) % mod)
-            if i == '1':
-                value = ((value * x) % mod)
+            value = (value * value) % mod
+            if i == "1":
+                value = (value * x) % mod
         return value
 
     # Random Number Generator
@@ -83,19 +84,20 @@ class RSA:
             if gcd == 1 and d > 0:
                 # print(f'\nPublic Key: {hex(n), hex(e)}', f'Private Key: {hex(d)}', sep="\n")
                 return [n, e], [d]
-    
+
     # Command to show the Primary and Public key to the console.
     def showKeys(self):
-        print( f'\n\tPubilic Key : {hex(self.key[0][0])}\n\tPrivate Key {hex(self.key[1][0])}')
+        print(
+            f"\n\tPubilic Key : {hex(self.key[0][0])}\n\tPrivate Key {hex(self.key[1][0])}"
+        )
         return [hex(self.key[0][0]), hex(self.key[0][1])], hex(self.key[1][0])
 
     # The encryption algorithm (text file) => encrypted text file.
     def encrypt(self, textFile):
-
         assert textFile, "No file found"
 
         # Grabs the public key from the initialized key generated value.
-        pub_key = self.key[0] 
+        pub_key = self.key[0]
 
         #  Parses the filename and extension from the inputed file.
         file_name, ext = textFile.split(".") if "." in textFile else (textFile, "txt")
@@ -104,27 +106,37 @@ class RSA:
         f = open(textFile).read()
 
         # Creates the document to create the encrypted version.
-        encrypted_f = open(f'{file_name}-encr.{ext}', "w+")
+        encrypted_f = open(f"{file_name}-encr.{ext}", "w+")
 
         # Creates the document path folder in the local directory.
-        if not os.path.exists(f'encryption-keys/{file_name}/'):
-            os.makedirs(os.path.dirname(f'encryption-keys/{file_name}/'))
+        if not os.path.exists(f"/encryption-keys/{file_name}/"):
+            os.makedirs(
+                os.path.dirname(f"{self.currentDir}/encryption-keys/{file_name}/")
+            )
 
-        # Creates the directories for the keys to local directory in 'encryption-keys/filename' directory.
-        encrypted_pub_k = open(f'encryption-keys/{file_name}/{file_name}-pub.key', "w+")
-        encrypted_priv_k = open(f'encryption-keys/{file_name}/.{file_name}-priv.key', "w+")
+        # Creates the sub directories for the keys to local directory in 'encryption-keys/filename' directory.
+        encrypted_pub_k = open(
+            f"{self.currentDir}/encryption-keys/{file_name}/{file_name}-pub.key", "w+"
+        )
+
+        encrypted_priv_k = open(
+            f"{self.currentDir}/encryption-keys/{file_name}/.{file_name}-priv.key", "w+"
+        )
 
         # Lists each characters encryption number in hex format.
         res = [hex(self.squareMultiply(ord(i), pub_key[1], pub_key[0]))[2:] for i in f]
         # print(bytes.fromhex(self.cache[0]).decode())
-        
 
         # Joins the cache files together and writes them to the new created encrypted file.
         encrypted_f.write(" ".join(res))
 
         # Writes the keys to the file in the .ssh/filename directory.
-        encrypted_pub_k.write(f'{("-" * 15)} Public Key {("-" * 15)} \n{hex(self.key[0][0])}')
-        encrypted_priv_k.write(f'{("-" * 15)} Priivate Key {("-" * 15)} \n{hex(self.key[1][0])}')
+        encrypted_pub_k.write(
+            f'{("-" * 15)} Public Key {("-" * 15)} \n{hex(self.key[0][0])}'
+        )
+        encrypted_priv_k.write(
+            f'{("-" * 15)} Priivate Key {("-" * 15)} \n{hex(self.key[1][0])}'
+        )
 
         # Closes the open files.
         encrypted_f.close()
@@ -135,54 +147,61 @@ class RSA:
 
     # The decryption algorithm (text encrypted file) => decrypted text file.
     def decrypt(self, enc_file=None):
-
         assert enc_file, "No encryption file found"
 
         #  Parses the filename and extension from the inputed file.
         file_name, ext = enc_file.split(".") if "." in enc_file else (enc_file, "txt")
         file_name = file_name.split("-")[0]
 
-
         # Opens the inputed file.
         f = open(enc_file).read()
 
         # Creates the file for the decrypted file.
-        decrypted_f = open(f'{file_name}-decr.{ext}', "w+")
+        decrypted_f = open(f"{self.currentDir}/{file_name}-decr.{ext}", "w+")
 
         # Opens the keys from the .ssh directory.
-        pub_key = open(f'encryption-keys/{file_name}/{file_name}-pub.key', "r+").readlines()[1]
-        priv_key = open(f'encryption-keys/{file_name}/.{file_name}-priv.key', "r+").readlines()[1]
+        pub_key = open(
+            f"{self.currentDir}/encryption-keys/{file_name}/{file_name}-pub.key", "r+"
+        ).readlines()[1]
+        priv_key = open(
+            f"{self.currentDir}/encryption-keys/{file_name}/.{file_name}-priv.key", "r+"
+        ).readlines()[1]
 
         # Reads the encrypted file and runs the power modular function against the public and private keys.
-        res = [chr(self.squareMultiply(eval("0x" + i), eval(priv_key), eval(pub_key))) for i in f.split(" ")]
+        res = [
+            chr(self.squareMultiply(eval("0x" + i), eval(priv_key), eval(pub_key)))
+            for i in f.split(" ")
+        ]
 
         # Writes to the decrypted file.
         decrypted_f.write("".join(res))
 
         # Closes the open files.
         decrypted_f.close()
-        
+
         return res
 
+
 if __name__ in ["__main__", "__live_coding__"]:
+    currentDir = re.sub(r"/[^/]*\.py$", "", __file__)
 
     # 80-bits is the highest my computer is able to run with a reasonable time.
     rsa = RSA(64)
 
-    # Variable created to encrypt this this file before.
-
+    # Variable created to encrypt this file before.
     res = input("[1] Encrypt\n[2] Decrypt: ")
     if res == "1":
         text = input("Enter full file name and ext for encryption: ")
-        assert os.path.exists(f'{text}'), "No file found!"
+        path = f"{text}"
+        assert os.path.exists(f"{currentDir}/{path}"), "No file found!"
 
         # Run the encryption schema on the text file.
-        cipher = rsa.encrypt(text)
+        cipher = rsa.encrypt(path)
 
     else:
         # variable created to store the the newly created encryption filename.
-        
+
         encr = input("Enter full file name and ext for decryption: ")
-        assert os.path.exists(f'{encr}'), "No file found!"
+        assert os.path.exists(f"{encr}"), "No file found!"
         # Run the decryption on encrypted file.
         plaintext = rsa.decrypt(encr)
